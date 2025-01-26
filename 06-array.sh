9cd7a49e87611995e52835f9f5a1647e2663e8c4
PrudhviKundan <prudhvisoft91@gmail.com>
2025-01-26 22:48:14 +0530

    modify 06-array.sh

--- 06-array.sh
+++ 06-array.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-FRUITS=("Apple" "Pineapple" "Mango") #array
+FRUITS=(Apple Pineapple Mango) #array
 echo "First Fruit is : ${FRUITS[0]}"
 echo "Second Fruit is : ${FRUITS[1]}"
 
 echo "List of Fruits are : ${FRUITS[@]}"
 echo "Count No.Of Elements in an array : ${#FRUITS[@]}"
-
-echo "Delete individual array element :  ${unset FRUITS[1]}"
+$Delete=${FRUITS[1]}
+echo "Delete individual array element :  ${FRUITS[@]/$Delete}"
+echo "After delete ceount No.Of Elements in an array : ${#FRUITS[@]}"
