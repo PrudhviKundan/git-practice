73af5e01feb76dd35c7e34cc0d50a233f93f9c14
PrudhviKundan <prudhvisoft91@gmail.com>
2025-01-26 22:17:48 +0530

    09-specialvariable.sh

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#special variable are @, #, 0
+#special variable are @, #, 0 , pwd
 echo "All variables passed to the script : $@"
 echo "Number of variable passed : $#"
 echo "Script name : $0"
+echo "Current working directory : $pwd"
