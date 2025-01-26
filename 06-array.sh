7fb9c9436d21bb7366dd0fc6469c3fd2e60febad
PrudhviKundan <prudhvisoft91@gmail.com>
2025-01-26 20:42:28 +0530

    update array file

--- 06-array.sh
+++ 06-array.sh
@@ -4,4 +4,4 @@ FRUITS=("Apple" "Pineapple" "Mango") #array
 echo "First Fruit is : ${FRUITS[0]}"
 echo "Second Fruit is : ${FRUITS[1]}"
 
-echo "List of Fruits are : ${fruits[@]}"
+echo "List of Fruits are : ${FRUITS[@]}"
