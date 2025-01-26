14a48a9a2497173fc2d9639f940fdaf2465ee34d
PrudhviKundan <prudhvisoft91@gmail.com>
2025-01-26 21:20:41 +0530

    modify datatype.sh

--- 08-datatype.sh
+++ 08-datatype.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-Number1=$1
-Number2=$2
+NUMBER1=$1
+NUMBER2=$2
 
-SUM=$(($Number1+$Number2))
+SUM=$(($NUMBER1+$NUMBER2))
 
-echo "TOTAL OF $Number1 and $Number2 is : $SUM"
+echo "TOTAL OF $NUMBER1 and $NUMBER2 is : $SUM"
