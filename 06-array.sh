d5b9c6c9216193a4e71d7fca55e8967c031fc300
PrudhviKundan <prudhvisoft91@gmail.com>
2025-01-26 22:56:48 +0530

    modify 06-array.sh

--- 06-array.sh
+++ 06-array.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-FRUITS=(Apple Pineapple Mango) #array
+FRUITS=(Apple Carrot Mango) #array
 echo "First Fruit is : ${FRUITS[0]}"
 echo "Second Fruit is : ${FRUITS[1]}"
 
 echo "List of Fruits are : ${FRUITS[@]}"
 echo "Count No.Of Elements in an array : ${#FRUITS[@]}"
-DELETE =unset FRUITS[1]
-echo "Delete individual array element :  ${DELETE[@]}"
+# Element to delete
+Non_Fruit=${FRUITS[1]}
+
+# Create a new array excluding the non-fruit to delete
+ACTUAL_FRUITS=()
+for FRUITS in "${ACTUAL_FRUITS[@]}"; do
+  if [[ $FRUITS != "$Non_Fruit" ]]; then
+    ACTUAL_FRUITS+=("$FRUITS")
+  fi
+done
+
+# Print the updated fruits
+echo "Updated ACTUAL FRUITS: ${ACTUAL_FRUITS[@]}"
 echo "After delete ceount No.Of Elements in an array : ${#FRUITS[@]}"
