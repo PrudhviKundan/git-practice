231252059bfae2a204ade4fcf1e5b923f7d8aedc
PrudhviKundan <prudhvisoft91@gmail.com>
2025-01-27 10:09:19 +0530

    modify 11-installation.sh

--- 11-installation.sh
+++ 11-installation.sh
@@ -8,5 +8,18 @@ then
     echo "please run this script with root access"
     exit 1
 fi
-dnf install git -y
+dnf list installed git
 
+if [ $? - ne 0 ]
+then
+    echo "Git is not installed, going to install it.."
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then 
+        echo "Git installation is not success... check it"
+        exit 1
+    else
+        echo "Git installation is success"
+else
+    echo "Git is already installed, nothing to do.."
+fi
