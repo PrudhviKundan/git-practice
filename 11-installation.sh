f7cce68468dadfd5eb730809241355b3c0623601
PrudhviKundan <prudhvisoft91@gmail.com>
2025-01-27 09:31:56 +0530

    modify 11-installation.sh

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# USERID=$(id -u)
-# echo "User ID is : $USERID"
+USERID=$(id -u)
+echo "User ID is : $USERID"
 
-# if [ $USERID -ne 0 ]
-# then 
-#     echo "please run this script with root access"
-# fi
-# dnf install git -y
+if [ $USERID -ne 0 ]
+then 
+    echo "please run this script with root access"
+    exit 1
+fi
+dnf install git -y
 
-ls -ltr
-
-lsdrt -ltr
-
-ls -l
