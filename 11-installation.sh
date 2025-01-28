030934dff5aaa4e819fd2de529f5e77092ceb4ba
PrudhviKundan <prudhvisoft91@gmail.com>
2025-01-28 23:28:54 +0530

    updated 11-installation.sh

--- 11-installation.sh
+++ 11-installation.sh
@@ -27,11 +27,11 @@ fi
 
 dnf list installed mysql
 
-if[ $? -ne 0 ]
+if [ $? -ne 0 ]
 then
     echo "MySQL is not installed.. going to install"
     dnf install mysql -y
-    if[ $? -ne 0 ]
+    if [ $? -ne 0 ]
     then
         echo "MySQL installation is failure.. please check"
         exit 1
