ebf89ec559f025ff6429621a5eedea82b068bf58
PrudhviKundan <prudhvisoft91@gmail.com>
2025-01-28 22:20:18 +0530

    updated 11-installation.sh

--- 11-installation.sh
+++ 11-installation.sh
@@ -13,7 +13,7 @@ dnf list installed git
 if [ $? - ne 0 ]
 then
     echo "Git is not installed, going to install it.."
-    dnf install git -y
+    dnf install gittt -y
     if [ $? -ne 0 ]
     then 
         echo "Git installation is not success... check it"
